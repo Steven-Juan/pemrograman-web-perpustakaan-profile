aeb0941896dbfaf04b9f255651150e5c7dc7da69
Steven-Juan <stevjuan00@gmail.com>
2025-09-29 01:06:33 +0700

    Save local changes before rebase

--- app/Livewire/Buku/DetailBuku.php
+++ app/Livewire/Buku/DetailBuku.php
@@ -4,20 +4,46 @@ namespace App\Livewire\Buku;
 
 use App\Models\Buku;
 use Livewire\Component;
+use Spatie\SchemaOrg\Schema;
 
 class DetailBuku extends Component
 {
     public $buku;
 
+    /**
+     * Mount the component.
+     *
+     * @param  int  $id
+     * @return void
+     */
     public function mount($id)
     {
         $this->buku = Buku::with(['kategori', 'image'])->findOrFail($id);
     }
 
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     */
     public function render()
     {
-        return view('livewire.buku.detail-buku', [
-            'buku' => $this->buku,
-        ]);
+        seo()
+            ->title($this->buku->judul_buku)
+            ->description(str($this->buku->deskripsi)->limit(160))
+            ->canonical(route('buku.show', $this->buku->id))
+            ->addSchema(
+                Schema::book()
+                    ->name($this->buku->judul_buku)
+                    ->description($this->buku->deskripsi)
+                    ->image($this->buku->image?->url)
+                    ->author($this->buku->penulis)
+            );
+
+        if ($this->buku->image) {
+            seo()->image($this->buku->image->url);
+        }
+
+        return view('livewire.buku.detail-buku');
     }
 }
